a5db4198dba1eeee23d9c8b66231c54bbb6afa7b
mesi <mesicharida202@gmail.com>
2024-12-04 21:42:32 +0900

    Refactor bookshelf module to use relative imports and update book type references

--- src/library/bookshelf.rs
+++ src/library/bookshelf.rs
@@ -1,7 +1,7 @@
-use super::book;
+use super::book::Book;
 
 pub struct Bookshelf {
-    books: Vec<book::Book>
+    books: Vec<Book>
 }
 
 impl Bookshelf {
@@ -10,22 +10,22 @@ impl Bookshelf {
     }
 
     // 本を追加するメソッド
-    pub fn add_book(&mut self, book: book::Book) {
+    pub fn add_book(&mut self, book: Book) {
         self.books.push(book);
     }
 
     // タイトルで本を検索するメソッド
-    pub fn search_books(&self, title_query: &str) -> Vec<&book::Book> {
+    pub fn search_books(&self, title_query: &str) -> Vec<&Book> {
         todo!("Implement `Bookshelf::search_books`");
     }
 
     // 本を本棚から取り出すメソッド
-    pub fn remove_book(&self, book: &super::book::Book) -> Option<book::Book> {
+    pub fn remove_book(&self, book: &super::book::Book) -> Option<Book> {
         todo!("Implement `Bookshelf::remove_book`");
     }
 
     // 本棚の本をすべて取り出すメソッド
-    pub fn take_all_books(&mut self) -> Vec<book::Book> {
+    pub fn take_all_books(&mut self) -> Vec<Book> {
         todo!("Implement `Bookshelf::take_all_books`");
     }
 }
