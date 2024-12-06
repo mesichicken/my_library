12bdad150fbdd3dbf087a0030d8474d62311924b
mesi <mesicharida202@gmail.com>
2024-12-06 22:34:25 +0900

    Refactor bookshelf module to use fuzzy-matcher for book title search

--- src/library/bookshelf.rs
+++ src/library/bookshelf.rs
@@ -1,12 +1,16 @@
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
+
 use super::book::Book;
 
 pub struct Bookshelf {
-    books: Vec<Book>
+    books: Vec<Book>,
+    matcher: SkimMatcherV2,
 }
 
 impl Bookshelf {
     pub fn new() -> Self {
-        Self { books: Vec::new() }
+        let matcher = SkimMatcherV2::default();
+        Self { books: Vec::new(), matcher: matcher }
     }
 
     // 本を追加するメソッド
@@ -16,7 +20,7 @@ impl Bookshelf {
 
     // タイトルで本を検索するメソッド
     pub fn search_books(&self, title_query: &str) -> Vec<&Book> {
-        todo!("Implement `Bookshelf::search_books`");
+        self.books.iter().filter(|book| self.matcher.fuzzy_match(&book.title, title_query).is_some()).collect()
     }
 
     // 本を本棚から取り出すメソッド
@@ -27,5 +31,22 @@ impl Bookshelf {
     // 本棚の本をすべて取り出すメソッド
     pub fn take_all_books(&mut self) -> Vec<Book> {
         todo!("Implement `Bookshelf::take_all_books`");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Book, Bookshelf};
+    #[test]
+    fn test_bookshelf() {
+        let mut shelf = Bookshelf::new();
+        let book1 = Book::new("すごいぞChatGPT!AIを使って学ぼうRust!", "山田太郎");
+        let book2 = Book::new("Pythonプログラミング入門", "山田花子");
+
+        shelf.add_book(book1);
+        shelf.add_book(book2);
+
+        let fonud_books = shelf.search_books("chatgpt");
+        println!{"{:?}", fonud_books};
     }
 }
